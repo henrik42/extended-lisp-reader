c46bcd17825c3213a20fd91b2860e4ca14f6ecdc
Henrik Heine <henrikheine@web.de>
2015-01-20 14:08:47 +0000

    set up project.clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject extended-lisp-reader "0.1.0-SNAPSHOT"
+  :description "Extend the Clojure LispReader with non-Lisp-ish DSLs"
+  :url "https://github.com/henrik42/extended-lisp-reader"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  ;;:dependencies [[org.clojure/clojure "1.5.1"]]
+  ;;:main ^:skip-aot extended-lisp-reader.core
+  :target-path "target/%s"
+  ;;:profiles {:uberjar {:aot :all}}
+  )
---
+++ src/extended_lisp_reader/core.clj
@@ -0,0 +1,2 @@
+(ns extended-lisp-reader.core)
+
