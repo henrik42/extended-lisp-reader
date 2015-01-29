28da7b401c9d6696719a10c70ab6f3dec78d5db2
Henrik Heine <henrikheine@web.de>
2015-01-29 07:35:03 +0000

    renamed namespaces

--- src/extended_lisp_reader/example.clj
+++ src/extended_lisp_reader/example.clj
@@ -1,23 +1,23 @@
 (ns extended-lisp-reader.example
   (:require [extended-lisp-reader.core]
-            [extended-lisp-reader.parsing :as parsing]
-            [extended-lisp-reader.instaparse :as insta]))
+            [extended-lisp-reader.stream-parser :as stream-parser]
+            [extended-lisp-reader.instaparse-adapter :as insta]))
 
-(def cfg (partial parsing/parse! insta/cfg-parser-for))
+(def cfg (partial stream-parser/parse! insta/cfg-parser-for))
                    
 ;; Die Funktion, die in #[<func> ...] referenziert wird, muss den Reader konsumieren.
-;; Daher kann man nicht direkt die Parsing-Fn verwenden, sondern muss die
+;; Daher kann man nicht direkt die Stream-Parser-Fn verwenden, sondern muss die
 ;; Generator-Fn parse! drumwickeln.
-(def sql (partial parsing/parse! (insta/parser-for "sql")))
+(def sql (partial stream-parser/parse! (insta/parser-for "sql")))
 
 ;; TODO: der Wert von #[sql ...] muss noch via Post-Walk auf eine (...) Form
 ;; abgebildet werden, damit der Compiler anschließend auch was damit macht.
 
 ;; So definiert man die Grammatik über die API
-(def ab1 (partial parsing/parse! (insta/cfg-parser-for "s = 'a'* 'b'*")))
+(def ab1 (partial stream-parser/parse! (insta/cfg-parser-for "s = 'a'* 'b'*")))
 
 ;; und so definiert man die Grammatik "inline"
-(def ab2 (partial parsing/parse! #[cfg s = 'a'* 'b'*]))
+(def ab2 (partial stream-parser/parse! #[cfg s = 'a'* 'b'*]))
 
 ;; run via
 ;; lein run -m extended-lisp-reader.example
---
+++ src/extended_lisp_reader/instaparse_adapter.clj
@@ -0,0 +1,69 @@
+(ns extended-lisp-reader.instaparse-adapter
+  (:require [clojure.java.io :as io]
+            [instaparse.core :as insta]
+            [instaparse.cfg :as cfg]
+            [instaparse.reduction :as reduction]
+            [instaparse.gll :as gll]))
+
+(defn parse [parser text]
+  "Internal parser function generator. Can be used to generate parser
+   function for ```extended-lisp-reader.stream-parser/parse!``` via
+   ```partial```.
+
+   Applies the instaparse parser to the text. If the parser returns an
+   ```instaparse.failure/failure?``` nil is returned. Otherwise the
+   parser's result (AST) is returned.
+
+   Note that the parser should use ```:total true``` in order to be
+   able to return an ```instaparse.failure/failure?``` and not throw
+   an exception when a complete successfull parse is not possible.
+   "
+  (let [ast (parser text)
+        res (when-not (insta/failure? ast) ast)]
+    res))
+
+(defn grammar-for [grammar-id]
+  "Internal grammar loading function.
+   Loads resource ```<grammar-id>.bnf``` and throws exception if it
+   cannot be found.
+  "
+  (let [r (str grammar-id ".bnf")]
+    (or (io/resource r)
+        (throw (RuntimeException. (format "Grammar file '%s' not found." r))))))
+
+(defn parser-for [grammar-id]
+  "Loads the grammar via ```grammar-for``` and builds an instaparse
+   parser for it.
+
+   Returns a parser function based on this parser which can be passed
+   to ```extended-lisp-reader.stream-parser/parse!```.
+   "
+  (let [grammar (grammar-for grammar-id)
+        parser (insta/parser grammar :string-ci true :total true)]
+    (partial parse parser)))
+
+(defn cfg-parser-for [spec-string]
+  "A parser function that parses instaparse grammars and can be passed
+   to ```extended-lisp-reader.stream-parser/parse!``` (i.e. it returns nil
+   if it cannot find a parse).
+
+   It returns a parser function which parses the specified language.
+
+   In contrast to ```parser-for``` this returned parser function does
+   not return the AST of the parsed input but - again - **a parser
+   function** that can be passed to
+   ```extended-lisp-reader.stream-parser/parse!```.
+  "
+  ;; Original code taken from instaparse/build-parser
+  (let [rules (gll/parse cfg/cfg :rules spec-string false)]
+    (if (instance? instaparse.gll.Failure rules) nil
+        (let [productions (map cfg/build-rule rules)
+              start-production (first (first productions))
+              parser (insta/map->Parser
+                      {:grammar (cfg/check-grammar
+                                 (reduction/apply-standard-reductions
+                                  :hiccup
+                                  (into {} productions)))
+                       :start-production start-production
+                       :output-format :hiccup})]
+          (partial parse parser)))))
